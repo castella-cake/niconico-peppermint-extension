748fb2ab0f90542dd311e2df54c77af009a38c33
CYakigasi <65476384+castella-cake@users.noreply.github.com>
2023-01-09 14:46:17 +0900

    CSSの適用方法を若干変更

--- js/watch.js
+++ js/watch.js
@@ -2,25 +2,16 @@
 function onError(error) {
     console.log(`Error: ${error}`);
 }
+function appendCSS(cssfile) {
+    $('body').append( $('<link>').attr( {'rel': 'stylesheet','href': cssfile} ) );
+}
 var getting = browser.storage.sync.get(null);
 getting.then(createCSSRule, onError);
 function createCSSRule(result) {
     if (result.playertheme == "hrzkv1") {
-        console.log(`CSS Loaded!`)
-        var link_style = $('<link>').attr(
-            {
-                'rel': 'stylesheet',
-                'href': browser.runtime.getURL("pagemod/css/playerstyle/harazyuku.css"),
-            },
-        );
-        $('body').append(link_style);
-        var link_style = $('<link>').attr(
-            {
-                'rel': 'stylesheet',
-                'href': browser.runtime.getURL("pagemod/css/playertheme/harazyuku.css"),
-            },
-        );
-        $('body').append(link_style);
+        console.log(`CSS Loaded!`);
+        appendCSS(browser.runtime.getURL("pagemod/css/playerstyle/harazyuku.css"));
+        appendCSS(browser.runtime.getURL("pagemod/css/playertheme/harazyuku.css"));
         $('.ControllerContainer-inner').css('top', '3px');
     } else {
         console.log(`CSS not Loaded!`)
