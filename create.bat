51ccf757940bcc3efd4474b30954df23b8eed17c
CYakigasi <65476384+castella-cake@users.noreply.github.com>
2023-07-27 13:53:52 +0900

    Gulpの準備

--- create.bat
+++ create.bat
@@ -1,21 +1,23 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 
-set /p version=バージョン名を入力: 
+set /p version=Enter version name: 
 del .\build\!version!
 mkdir .\build\!version!
 
-echo Firefox用のフォルダーを作成中...
+echo Compiling Stylus-lang stylesheets
+start /WAIT /b stylus pagemod/css/darkmode
+echo Creating files for Firefox
 mkdir .\build\!version!\firefox
 robocopy .\ .\build\!version!\firefox\ /s /e /xd "build" ".git" /xf ".gitignore" "create.bat"
 del .\build\!version!\firefox\manifest_chrome.json
 rem Powershell Compress-Archive -Path .\build\!version!\firefox\* -DestinationPath .\build\!version!\niconico-peppermint.zip
 rename .\build\!version!\niconico-peppermint.zip niconico-peppermint.xpi
-echo Chrome用のファイルを作成中...
+echo Creating files for Chromium
 mkdir .\build\!version!\chrome
 robocopy .\ .\build\!version!\chrome\ /s /e /xd "build" ".git" /xf ".gitignore" "create.bat"
 del .\build\!version!\chrome\manifest.json
 rename .\build\!version!\chrome\manifest_chrome.json manifest.json
 rem Powershell Compress-Archive -Path .\build\!version!\chrome\ -DestinationPath .\build\!version!\niconico-peppermint-chrome.zip
-echo completed!
+echo Done
 pause
