1e3ba6bdee62aca21233bd7b100c5bd890a41a67
castella-cake <65476384+castella-cake@users.noreply.github.com>
2024-02-27 19:42:58 +0900

    イースターエッグにアニメーションを追加

--- src/pages/credit.js
+++ src/pages/credit.js
@@ -35,10 +35,40 @@ document.addEventListener("DOMContentLoaded", function () {
         if (eggArray.join("") == "star") {
             document.getElementById("ee_style").innerHTML = `
                 body { background: #000; color: #ff0; }
+                .maincontainer {
+                    overflow: hidden;
+                }
+                .maincontainer_inner {
+                    transform-style: preserve-3d;
+                    animation: 10s linear 0s forwards star;
+                    overflow: hidden;
+                }
                 .settings-area { background: transparent; text-align: center; }
                 .settings-area > * { text-align: center; font-family: "Arial", "Helvetica", "Helvetica Neue", "Meiryo", sans-serif }
                 .settings-area h1 { text-transform: uppercase; }
                 .settings-area a { color: #ff0; }
+                @keyframes star {
+                    0% {
+                        transform: perspective(200px) rotateX(10deg) scale(0.7) translateY(90vh);
+                        opacity: 1.0
+                    }
+                    97% {
+                        transform: perspective(200px) rotateX(10deg) scale(0.7) translateY(-150vh);
+                        opacity: 1.0
+                    }
+                    98% {
+                        transform: perspective(200px) rotateX(10deg) scale(0.7) translateY(-150vh);
+                        opacity: 0.0
+                    }
+                    99% {
+                        transform: perspective(0px) rotateX(0deg) scale(1) translateY(0vh);
+                        opacity: 0.0
+                    }
+                    100% {
+                        transform: perspective(0px) rotateX(0deg) scale(1) translateY(0vh);
+                        opacity: 1.0
+                    }
+                }
             `
         }
     })
