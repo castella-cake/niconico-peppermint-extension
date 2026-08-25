d6d6568b4e7c3823f48959769d05bcfc07bbd5f4
castella-cake <65476384+castella-cake@users.noreply.github.com>
2026-08-25 21:08:37 +0900

    FIX: isNvpcNextにrecommendationsを含めてロジックを統一

--- utils/isNvpcNext.ts
+++ utils/isNvpcNext.ts
@@ -1,4 +1,4 @@
-const targetPathnames = ["/watch/", "/ranking", "/tag/", "/tag_shorts/", "/search/", "/search_shorts/", "/series_search/", "/mylist_search/", "/user_search/"] 
+const targetPathnames = ["/watch/", "/ranking", "/tag/", "/tag_shorts/", "/search/", "/search_shorts/", "/series_search/", "/mylist_search/", "/user_search/", "/recommendations", "/shorts/"] 
 
 export function isNvpcNext(location: Location): boolean {
     return location.hostname === "www.nicovideo.jp" && targetPathnames.some(path => location.pathname.startsWith(path))
